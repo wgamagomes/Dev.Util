e271ef40ee8b3bbd6d6e5c01aadb26046c6563a6
wellington <wellington.gomes@hbsis.com.br>
2018-05-14 11:19:15 -0300

    [ADD] Solution

---
+++ String.Util/StringExtention.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace String.Util
+{
+     public static class StringExtension
+    {
+       // Convert Whitespaces to  To Single Space
+        public static string ToSingleSpace(this string val)
+        {
+			if (val == null)
+                throw new ArgumentNullException(nameof(val));
+			
+            return Regex.Replace(val, @"\s+", " ");
+        }
+		
+		public static string OnlyNumberDigits(this string val)
+        {
+            if (val == null)
+                throw new ArgumentNullException(nameof(val));
+
+            return new string(val.Where(Char.IsDigit).ToArray());
+        }
+    }
+}
